1387aaaf1ce360621dcd10b3fb434c3f8e404c9c
PRIYANSHU SINGH <sentysingh456@gmail.com>
2026-02-12 12:56:55 +0530

    Defining custom method in repository, using JPA to extend its properties

--- patient-service/src/main/java/com/sentysingh/patient_service/repository/PatientRepository.java
+++ patient-service/src/main/java/com/sentysingh/patient_service/repository/PatientRepository.java
@@ -9,4 +9,5 @@ import java.util.UUID;
 // just by extending it to Jparespository we have all cruds available to us
 @Repository
 public interface PatientRepository extends JpaRepository <Patient, UUID> {
+    boolean existsByEmail(String email);
 }
